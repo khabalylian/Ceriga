e7906a09fe8a7af3eba35a1065845b6d67b18848
Khabal Yulian <habalulian@gmail.com>
2024-09-11 12:55:54 +0300

    #delete page

--- src/app/design/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Hom() {
-  return (
-    <div>
-      <h2>ghfhfgh</h2>
-    </div>
-  );
-}
--- src/pages/design.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h2>gfhfghfghf</h2>
-    </div>
-  );
-}
